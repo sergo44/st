c4a64a0bf27e14c2c0959c3dd90435adf52970de
Sergey Oskorbin <sergo44@mail.ru>
2024-09-05 15:43:01 +0800

    Изменены настройки в соответствии с текущими настройками Docker

--- www/src/config.local.php
+++ www/src/config.local.php
@@ -1,8 +1,8 @@
 <?php
 const ST_DB_HOST = "mariadb";
-const ST_DB_USER = "st_user";
+const ST_DB_USER = "user";
 const ST_DB_PASS = "hW5SrXpFGuMMGq44";
-const ST_DB_NAME = "st_db";
+const ST_DB_NAME = "db";
 const ST_DEFAULT_REDIS_HOST =  "redis";
 const ST_DEFAULT_REDIS_PORT =  0;
 const ST_DEFAULT_REDIS_TIMEOUT =  3.0;
